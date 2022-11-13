86b8826eff459dc17684abda7133463ffc3a27d4
Faiz khan <98544944+hellofaiz@users.noreply.github.com>
2022-11-13 13:43:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div className="container">
+      <div className="About_us">
+        <h1>About Us</h1>
+        <p>At Zappos.com, Our purpose is simple: to live and deliver WOW.</p><br />
+
+        <p>Twenty year ago, we began as a small online retailer that only sold shoes. Today, we still sell shoes a as well as clothing, handbag, assesories, and more. That "more" is providing the very best customer service.Customer experience, and company culture. We aim to inspire the world by showing it's possible to simulteneously deliver happiness to customers, employees, vendors, shareholders,and the community in a long term, sustainable way.</p><br />
+        <p>We hope that in the future people won't even realize we started selling shoes online.Instead, they'll know Zappos as a service company that just happens to sell__________.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
